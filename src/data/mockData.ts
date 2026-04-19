a2d085a70b1c84b3e91a3c99b058502ca8c877ec
om607397-wq <om607397@gmail.com>
2026-04-19 16:51:22 +0200

    update posters

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -35,10 +35,15 @@ export const players: Player[] = [
   },
   {
     id: "p2",
-    name: "محمد العويس",
-    avatarUrl: "https://picsum.photos/seed/p2/200/200",
+    name: "معاذ جابر",
+    avatarUrl: "/public/imgs/moaz/1.jpg",
     posters: [
-      { id: "17", title: "تصدي رائع", imageUrl: "https://picsum.photos/seed/pos3/1059/1488", category: "حارس" },
+      { id: "17", title: "بوستر 1", imageUrl: "/imgs/moaz/1.jpg", category: "حارس" },
+      { id: "18", title: "بوستر 2", imageUrl: "/imgs/moaz/2.jpg", category: "حارس" },
+      { id: "19", title: "بوستر 3", imageUrl: "/imgs/moaz/3.jpg", category: "حارس" },
+      { id: "20", title: "بوستر 4", imageUrl: "/imgs/moaz/4.jpg", category: "حارس" },
+      { id: "21", title: "بوستر 5", imageUrl: "/imgs/moaz/5.jpg", category: "حارس" },
+      { id: "22", title: "بوستر 6", imageUrl: "/imgs/moaz/6.jpg", category: "حارس" },
     ],
   },
   {
